5b290a2f43ce6a4ec8ec67c59594d7ab313bee93
ramonruizrc <ramonruizrc@gmail.com>
2020-10-29 01:20:11 -0600

    aagregando php y subiendo al produccion

---
+++ trato-hecho.com/php/config.php
@@ -0,0 +1,7 @@
+<?php
+    define('DB_SERVER', 'mysql.trato-hecho.com');
+    define('DB_USERNAME', 'carviz1dreampres');
+    define('DB_PASSWORD', 'rKT9t8rq');
+    define('DB_DATABASE', 'carviz1_dream_press');
+    $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+?>
