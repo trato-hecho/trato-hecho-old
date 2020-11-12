645534c92bbf0ff266165dbdca9b253a6b79694d
ramonruizrc <ramonruizrc@gmail.com>
2020-11-12 14:00:02 -0600

    ain section html

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,23 +4,43 @@ import './Main.css'
 
 function Main(){
 
-
     return (
-        <main>
-            <section>
-                <h1>La plusvalía de un bien inmueble puede darte grande beneficios.</h1>
-                <form>
-                    <label></label>
-                    <input/>
-                    <label></label>
-                    <input/>
-                    <button></button>
-                </form>
+        <main className="main">
+            <section className="main__section">
+                <div className="main__section--container">
+                    <h1 className="main__section--title">La plusvalía de un bien inmueble puede darte grande beneficios.</h1>
+                </div>
+
+                <div className="main__section--container">
+                    <div className="main__section--search">
+                        <label className="main__section--search-label">Colonia</label>
+                        <select className="main__section--search-select">
+                            <option className="main__section--search-option">
+
+                            </option>
+                        </select>
+                    </div>
+                    <div className="main__section--search">
+                        <label className="main__section--search-label">Precio</label>
+                        <select className="main__section--search-select">
+                            <option className="main__section--search-option">
+
+                            </option>
+                        </select>
+                    </div>
+
+                    <div className="main__section--search">
+                        <button className="main__section--search-button">
+                            <img className="main__section--search-button-img" src="https://firebasestorage.googleapis.com/v0/b/voltaic-tuner-270719.appspot.com/o/icons%2Fsearch-icon.svg?alt=media&token=eb8b00b5-5069-4655-9815-e22b0b730b3f" />
+                        </button>
+                    </div>
+
+                </div>
+                
+
             </section>
             <section>
-                <button>
-                    <a></a>
-                </button>
+
             </section>
         </main>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../App.css';
 import Header from '../components/Header'
+import Main from '../components/Main'
+
 
 function Home() {
     return (
         <>
             <Header />
+            <Main />
         </>        
     );
 }
